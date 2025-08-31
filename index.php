fb19233fdd853a9c0b4e400f4e92fa499ff6b005
Orlando <variorlando1@gmail.com>
2025-08-31 23:45:17 +0200

    Pagina index - creacion del carrousel (responsive), creacion del container con la informacion (responsive) y creacion del footer

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-    <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="css/styles.css">
+    
 
     <title>Home</title>
 </head>
@@ -39,8 +40,66 @@
             </div>
         </nav>
     </header>
+    <main>
+        <div class="carousel">
+            <div class="slides" id="slides">
+                <div class="slide"><img src="img/notice1.jpg" alt="Slide 1"></div>
+                <div class="slide"><img src="img/notice2.jpg" alt="Slide 2"></div>
+                <div class="slide"><img src="img/notice3.jpg" alt="Slide 3"></div>
+            </div>
+
+            <button class="left-arrow" aria-label="left">
+                <i class="fa-solid fa-arrow-left"></i>
+            </button>
+            <button class="right-arrow" aria-label="right">
+                <i class="fa-solid fa-arrow-right"></i>
+            </button>
+            <button class="btn-show-news">SHOW NEWS</button>
+        </div>
+
+        <div class="container">
+            <div class="info-container">
+                <img src="img/image1.png" alt="imagen1">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                    Exercitationem pariatur quas sequi reprehenderit praesentium 
+                    fugiat a excepturi voluptatibus aspernatur accusantium, saepe 
+                    iste quidem molestiae aperiam. Sit numquam at quas iste?</p>
+            </div>
+            <div class="info-container">
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                    Exercitationem pariatur quas sequi reprehenderit praesentium 
+                    fugiat a excepturi voluptatibus aspernatur accusantium, saepe 
+                    iste quidem molestiae aperiam. Sit numquam at quas iste?</p>
+                    <img src="img/image2.png" alt="imagen1">
+            </div>
+            <div class="info-container">
+                <img src="img/image3.png" alt="imagen1">
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                    Exercitationem pariatur quas sequi reprehenderit praesentium 
+                    fugiat a excepturi voluptatibus aspernatur accusantium, saepe 
+                    iste quidem molestiae aperiam. Sit numquam at quas iste?</p>
+            </div>
+        </div>
+        
+    </main>
+
+    <footer>
+        <div class="footer-container">
+            <div class="social-media">
+                <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                <a href="#"><i class="fa-brands fa-youtube"></i></a>
+            </div>
+            <div class="info-footer">
+                <p>PONTE EN CONTACTO CON NOSOTROS 999 88 86 66</p>
+                <p>ESTAMOS EN CALLE SUIZA 24, 28052, MADRID, ESPAÑA</p>
+                <p>@DERECHOS DE AUTOR@</p>
+            </div>
+        </div>
+    </footer>
 
     <script src="./js/home.js"></script>
+    
 
 </body>
 
--- js/home.js
+++ js/home.js
@@ -1,6 +1,31 @@
 const btnMenuToggle = document.getElementById("menu-toggle");
 const divListOptionsMenu = document.getElementById("list-options-menu");
 
-btnMenuToggle.addEventListener("click", ()=>{
+const track = document.getElementById("slides");
+const slides = document.querySelectorAll(".slide");
+const prevBtn = document.querySelector(".left-arrow");
+const nextBtn = document.querySelector(".right-arrow");
+
+let currentSlide = 0;
+const total = slides.length;
+
+
+btnMenuToggle.addEventListener("click", () => {
     divListOptionsMenu.classList.toggle("active")
 });
+
+// deslizar a índice
+function goTo(index) {
+    currentSlide = (index + total) % total;
+    track.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+// botones
+prevBtn.addEventListener("click", () => goTo(currentSlide - 1));
+nextBtn.addEventListener("click", () => goTo(currentSlide + 1));
+
+// autoplay (opcional)
+let timer = setInterval(() => goTo(currentSlide + 1), 5000);
+
+// inicio
+goTo(0);
